ff9332a2e7faa11dfe0cd4cb5cf7672fe9e106ac
Kimball Hill <hillkimball@gmail.com>
2018-06-06 11:05:58 -0600

    changed ref to ShapeShifter

--- server/data_access/Dao.py
+++ server/data_access/Dao.py
@@ -3,7 +3,7 @@ import sqlite3
 import time
 from typing import List, Set, Dict, Any, Tuple
 
-import UseParquet as ss
+import ShapeShifter.ShapeShifter.UseParquet as ss
 from .Constants import *
 # from .Exceptions import RequestError
 from .Query import Query
