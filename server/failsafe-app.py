f33fb313f88265ad80a0531a5f862a94df69e9a1
Kimball Hill <hillkimball@gmail.com>
2018-10-26 11:15:07 -0600

    added deployment docker-compose

--- server/failsafe-app.py
+++ server/failsafe-app.py
@@ -6,4 +6,4 @@ def create_app():
   return app
 
 if __name__ == '__main__':
-    create_app().run(debug=True, host='0.0.0.0', port=9998)
+    create_app().run(debug=True, host='0.0.0.0', port=8889)
